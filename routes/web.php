f63821f9b850532942d471b3f9d733c9c061abb2
Abdul Hannan <abdulhannan1247@gmail.com>
2025-03-16 15:18:51 +0500

    Basic Controller

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ HomeController:: class, 'index']);
+Route::get('/about', [ HomeController:: class,'showAboutPage']);
 
-Route::get('about', function () {
-    return view('about');
-});
