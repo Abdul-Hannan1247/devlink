4ca7927e6bb8fef960d3ee6e11fb1d4ec776af16
Abdul Hannan <abdulhannan1247@gmail.com>
2025-03-16 18:09:02 +0500

    Models

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,14 @@
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SingleController;
+use App\Models\Blog;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ HomeController:: class, 'index']);
 Route::get('/about', [ HomeController:: class,'showAboutPage']);
 Route::get('/single-action',SingleController::class);
-Route::resource('/blog', BlogController::class);
+// Route::resource('/blog', BlogController::class);
+Route::get('/blog', function(){
+    $blogs = Blog::all();
+    dd($blogs);
+});
