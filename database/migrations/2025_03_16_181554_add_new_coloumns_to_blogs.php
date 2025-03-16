7412b0c79e68ddf8a71792537bd5c396b7ca4a39
Abdul Hannan <abdulhannan1247@gmail.com>
2025-03-16 23:17:56 +0500

    Seede/Factories

--- database/migrations/2025_03_16_181554_add_new_coloumns_to_blogs.php
+++ database/migrations/2025_03_16_181554_add_new_coloumns_to_blogs.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('blogs', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-            $table->text('description');
-            $table->timestamps();
+        Schema::table('blogs', function (Blueprint $table) {
+            $table->string('author')->nullable()->after('id');
         });
     }
 
@@ -24,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('blogs');
+        Schema::table('blogs', function (Blueprint $table) {
+            //
+        });
     }
 };
