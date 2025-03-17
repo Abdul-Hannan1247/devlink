36262ae1a02277987af9734e2c19a40a7955c357
Abdul Hannan <abdulhannan1247@gmail.com>
2025-03-17 10:49:24 +0500

    Query Builders

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     function index(){
+        // ########## Creating/inserting a data ##########
+        // DB::table('users')->insert(
+        //     [
+        //     'name' => 'Huma',
+        //     'email' => 'Huma@gmail.com',
+        //     'password' => '123456',
+        //     ]);
+        // ########## Retrieving the data  ############
+        // $user= DB::table('users')->where('id',4)->first();
+        // return $user;
+        // ########## Updating the Data############
+        // DB::table('users')->where('id',4)->update([
+        //     'name' => 'test'
+        // ]);
+        //########## Delete a row  ##########
+        // DB::table('users')->where('id',4)->delete();
+        // DB::table('users')->where('id','>',3)->delete();
+
+        //########## Retrieving a list of coloumn values ##########
+        // $blog=DB::table('blogs')->select('title')->get();
+        // dd($blog);
+
+        //  #### Agregates ####
+        // $products = DB::table('products')->min('price');
+        // dd($products);
+
+
         return view('welcome');
     }
 
