ec8bab27b384fa3df1cee7f22e317194e95f683b
Abdul Hannan <abdulhannan1247@gmail.com>
2025-03-18 17:32:50 +0500

    Forms and Validation | CSRF

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,14 +11,6 @@ use Illuminate\Support\Facades\DB;
 class HomeController extends Controller
 {
     function index(){
-
-    // Product::find(1)->delete();
-    // Product::withTrashed()->find(1)->restore();    // Restoring the soft deleted data.
-    $products= Product::withTrashed()->find(2);
-    $products->forceDelete();
-    dd($products);
-
-
         return view('welcome');
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,13 @@
 <?php
 
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SingleController;
 use App\Models\Blog;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ HomeController:: class, 'index']);
-Route::get('/about', [ HomeController:: class,'showAboutPage']);
-Route::get('/single-action',SingleController::class);
-// Route::resource('/blog', BlogController::class);
-Route::get('/blog', function(){
-    $blogs = Blog::all();
-    dd($blogs);
-});
+Route::get('contact', [ContactController::class,'index'])->name('contact.index');
+Route::post('contact', [ContactController::class,'contactSubmit'])->name('contact.submit');
+
