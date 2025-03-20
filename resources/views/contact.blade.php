25a0ed3944716de10c52e427a95ce8a3d14cec5d
Abdul Hannan <abdulhannan1247@gmail.com>
2025-03-20 08:05:27 +0500

    Forms and validation

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,6 +4,12 @@
     <section>
         <div class="row justify-content-center">
             <div class="col-md-6">
+                @if ($errors->any())
+                    @foreach ($errors->all() as $error )
+                      <div class="alert alert-danger">{{ $error }}</div>
+                    @endforeach
+                    
+                @endif
                 <div class="card mt-5 mb-5">
                  <div class="card-body">
                      <form action="{{ route('contact.submit') }}" method="POST">
@@ -11,19 +17,19 @@
                         @csrf
                          <div class="mb-3">
                              <label for="" class="form-label">Name</label>
-                             <input type="text" class="form-control" id="">
+                             <input type="text" class="form-control" id="" name="name" value="{{ old('name') }}">
                          </div>
                          <div class="mb-3">
                             <label for="" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="">
+                            <input type="email" class="form-control" id="" name="email" value="{{ old('email') }}">
                         </div>
                         <div class="mb-3">
                             <label for="" class="form-label">Subject</label>
-                            <input type="text" class="form-control" id="">
+                            <input type="text" class="form-control" id="" name="subject" value="{{ old('subject') }}">
                         </div>
                         <div class="mb-3">
                             <label for="" class="form-label">Message</label>
-                            <textarea name="" class="form-control"></textarea>
+                            <textarea name="message" class="form-control" >{{old('message')}} </textarea>
                         </div>
                          
                          
